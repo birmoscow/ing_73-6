805af1ca24c4ba3aee1bf54e4b8ee4351f222525
tubond <57327504+birmoscow@users.noreply.github.com>
2022-12-10 19:53:16 +0300

    Create uint_palindrome.cpp

---
+++ uint_palindrome.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  unsigned int num;
+  cout << "Введите число: ";
+  cin >> num;
+
+  unsigned int num_cpy = num, num_rev = 0;
+
+  while(num_cpy > 0) {
+    num_rev = num_rev * 10 + num_cpy % 10;
+    num_cpy /= 10;
+  }
+
+  if (num_rev == num) cout << "Палиндром";
+  else cout << "Не палиндром";
+  
+}
