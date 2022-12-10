73b0026515941fcf5ab29ef434235b894c7fcba6
tubond <57327504+birmoscow@users.noreply.github.com>
2022-12-10 20:29:42 +0300

    Update str_palindrome.cpp

--- str_palindrome.cpp
+++ str_palindrome.cpp
@@ -13,3 +13,28 @@ int main() {
   if (s == reversed_s) cout << "Палиндром";
   else cout << "Не палиндром";
 }
+
+
+/*
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  string s;
+  bool flag = true;
+  cin >> s;
+
+  for(int i = 0; i < s.size(); i++) {
+    if(s[i] != s[s.size() - i - 1]) {
+      flag = false;
+      break;
+    }
+  }
+
+  if (flag) cout << "Палиндром";
+  else cout << "Не палиндром";
+}
+
+*/
