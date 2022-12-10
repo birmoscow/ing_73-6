a9796bd0161b31400bc1a92d0bdaf806e5cc285f
tubond <57327504+birmoscow@users.noreply.github.com>
2022-12-10 20:41:33 +0300

    Create abc.cpp

---
+++ abc.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  string s;
+  int count = 0;
+  cin >> s;
+
+  for(int i = 0; i < s.size() - 2; i++) {
+    if(s[i] == 'a' && s[i + 1] == 'b' && s[i + 2] == 'c') {
+      count += 1;
+    }
+  }
+
+  cout << count;
+}
