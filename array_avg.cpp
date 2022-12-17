d4341493512119066b75980a1fc5d3e3404b8911
tubond <57327504+birmoscow@users.noreply.github.com>
2022-12-17 19:34:11 +0300

    Update array_avg.cpp

--- array_avg.cpp
+++ array_avg.cpp
@@ -21,3 +21,34 @@ int main() {
 
   cout << "Среднее арифметическое: " << (float)sum / size;
 }
+
+/*
+
+#include <iostream>
+
+using namespace std;
+
+void createArray(int arr[], int size) {
+  for(int i = 0; i < size; i++) {
+    arr[i] = i;
+  }
+}
+
+
+float averageArray(int arr[], int size) {
+  float sum = 0;
+  for(int i = 0; i < size; i++) {
+    sum += arr[i];
+  }
+  return sum / size;
+}
+
+
+int main() {
+  int *array = new int[5];
+  createArray(array, 5);
+
+  cout << averageArray(array, 5);
+}
+
+*/
